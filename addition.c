2e0728a9f91f70e639623b96f3d1b3f95267fb4b
JAINAM SHAH <jainamshah22@gmail.com>
2019-05-20 09:50:48 +0530

    Add files via upload

---
+++ addition.c
@@ -0,0 +1,12 @@
+/* Author - Para_sf */
+/* Program Addition*/
+#include<stdio.h>
+main()
+{
+int number;
+float amount;
+number = 100;
+amount = 30.75 + 75.35;
+printf("%d \n", number);
+printf("%5.1f", amount);
+}
