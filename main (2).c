ace7ef3041cc58660a7e9469ef434814d0914c9b
JAINAM SHAH <jainamshah22@gmail.com>
2019-05-20 10:33:38 +0530

    Add files via upload

---
+++ main (2).c
@@ -0,0 +1,16 @@
+/*Author para_sf*/
+/* Long integer */
+#include <stdio.h>
+
+int main()
+{
+int number;
+printf("Enter a number: \n");
+scanf("%d", &number);
+
+if (number<100)
+    printf("Your number is smaller than 100");
+else
+    printf("Your number is greater than 100");
+return 0;
+}
